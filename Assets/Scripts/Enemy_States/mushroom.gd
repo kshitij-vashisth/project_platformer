d4486e95b0df5ee35406a218b1efc13f98b4eeb3
Kshitij Vashisth <kshitijvashisth@gmail.com>
2026-08-09 10:17:25 +0000

    added bounce to player and squash, added squash sound

--- Assets/Scripts/Enemy_States/mushroom.gd
+++ Assets/Scripts/Enemy_States/mushroom.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
+@export var SPEED: float = 300.0
+@export var player_bounce_velocity: float = 400.0
+@export var sprite:AnimatedSprite2D
 
+@export var squash_sound: AudioStreamPlayer2D
+
+func squash() -> void:
+	squash_sound.play()
+	sprite.scale.y = 0.3
+	sprite.position.y += 20
 #func _ready() -> void:
 	#$Area2D.body_entered.connect(_on_area_2d_body_entered)
 	#print("Area2D connected, monitoring: ", $Area2D.monitoring)
@@ -21,7 +30,10 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		var y_delta = position.y - body.position.y
 		if y_delta > 30:
 			print("Destroy enemy")
+			body.velocity.y += -player_bounce_velocity
+			squash()
+			await get_tree().create_timer(0.2).timeout
 			queue_free()
-			body.jump()
+			
 		#print("Collision")
 		#print(y_delta)
