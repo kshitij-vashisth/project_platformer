e9d9ed55a94f13a92ad81e270795acc10c236358
Kshitij Vashisth <kshitijvashisth@gmail.com>
2026-08-12 07:47:32 +0000

    [feature] added UI canvas layer, added lives to UI, added pause menu

---
+++ Assets/Scripts/pause_screen.gd
@@ -0,0 +1,24 @@
+extends Node
+@onready var pause_panel: Panel = %PausePanel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var esc_pressed: bool = Input.is_action_just_pressed("pause")
+	if esc_pressed:
+		get_tree().paused == true
+		pause_panel.show()
+
+
+func _on_resume_button_pressed() -> void:
+	pause_panel.hide()
+	get_tree().paused == false
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Assets/Scenes/main_menu.tscn")
